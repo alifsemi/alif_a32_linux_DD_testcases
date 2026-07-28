1d5131808c9f35972108299140f3fc6508c88489
Ramprasad Gorai <ramprasad.gorai@alifsemi.com>
2026-07-28 16:18:37 +0530

    testcases: update license headers
    Update license headers
    
    Signed-off-by: Ramprasad Gorai <ramprasad.gorai@alifsemi.com>

--- testcases/cpufreq/cpu_benchmark.c
+++ testcases/cpufreq/cpu_benchmark.c
@@ -1,4 +1,11 @@
-/*
+/* Copyright (C) 2026 Alif Semiconductor - All Rights Reserved.
+ * Use, distribution and modification of this code is permitted under the
+ * terms stated in the Alif Semiconductor Software License Agreement
+ *
+ * You should have received a copy of the Alif Semiconductor Software
+ * License Agreement with this file. If not, please write to:
+ * contact@alifsemi.com, or visit: https://alifsemi.com/license
+ *
  * CPU-bound benchmark for CPUFreq validation
  * Compile: arm-poky-linux-musl-gcc -O0 -o cpu_benchmark cpu_benchmark.c
  * Usage: ./cpu_benchmark [iterations]
